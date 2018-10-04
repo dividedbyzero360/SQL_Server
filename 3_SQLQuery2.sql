27b6869476f27980d2f3845c2c951d4a27bd79be
Wastabir Rajvee Alam <wastabirrajveealamsa_cse10@its.edu.in>
2018-10-04 00:27:51 -0400

    Adding default constraint

--- 3_SQLQuery2.sql
+++ 3_SQLQuery2.sql
@@ -5,7 +5,7 @@ CREATE DATABASE DBLearning
 use DBLearning
 CREATE TABLE tblPerson
 (
-ID INT Primary key, -- By default primary keys are null
+ID INT Primary key, -- By default primary keys are not null
 Name nvarchar(50),
 Email nvarchar(50),
 GenderId INT
@@ -19,3 +19,6 @@ Gender nvarchar(50) NOT NUll
 
 ALTER TABLE tblPerson add constraint tblPerson_GenderId_FK
 FOREIGN KEY(GenderId) references tblGender (ID) 
+
+--A FOREIGN KEY constraint does not have to be linked only to a PRIMARY KEY constraint in another table;
+-- it can also be defined to reference the columns of a UNIQUE constraint in another table.
---
+++ 4_SQLQuery3.sql
@@ -0,0 +1,18 @@
+--ALTER TABLE TABLE_NAME ADD CONSTRAINT CONSTRAINT_NAME 
+--DEFAULT DEFAULT_VALUE FOR EXISTING_COLUMN_NAME
+
+--ALTER TABLE TABLE_NAME ADD COLUMN_NAME DATA_TYPE NULL|NOT NULL
+--CONSTRAINT {CONSTRAINT_NAME} DEFAULT DEFAULT_VALUE
+
+--ALTER TABLE TABLE_NAME DROP CONSTRAINT CONSTRAINT_NAME
+
+use DBLearning
+Insert into tblPerson (ID, NAme, Email) values (1,'Wastabir','wastabir.alam@gmail.com')
+
+Insert into tblGender values (1,'Male'), (2,'Female'),(3,'UnKnown')
+
+ALTER TABLE tblPerson ADD CONSTRAINT DF_GenderID 
+DEFAULT 3 FOR GenderId
+
+Insert into tblPerson (ID, NAme, Email) values (2,'Racktim','racktim.dutta@gmail.com')
+Insert into tblPerson (ID, NAme, Email,GenderId) values (3,'Satyajeet','Satyajeet.dowra@gmail.com',NULL)
